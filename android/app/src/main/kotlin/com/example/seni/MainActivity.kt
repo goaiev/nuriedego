4d23989b194af86543329fbd2839b6e6bab49078
goaiev <kit.bio206@gmail.com>
2020-12-23 17:50:21 +0900

    no message

--- android/app/src/main/kotlin/com/example/seni/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.seni
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/paint/components/paint_select_component.dart
+++
@@ -1,7 +0,0 @@
-class PaintSelect {
-  List<String> paintList = [
-    'images/pain1.png',
-    'images/pain2.png',
-    'images/pain3.png',
-  ];
-}
